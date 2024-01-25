0dded07ba43e55e461bf3b34ceb4c86ba337ee18
fatemeh <babaei.f1996@gmail.com>
2024-01-25 14:03:19 +0330

    feat: add routes component

---
+++ my-app/src/pages/index.js
@@ -0,0 +1,6 @@
+import All from "./All/All";
+import Active from "./Active/Active";
+import Pending from "./Pending/Pending";
+import Compeleted from "./Compeleted/Compeleted";
+
+export { All, Compeleted, Active, Pending };
