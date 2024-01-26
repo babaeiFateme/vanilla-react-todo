ab38da85f5bfc52235f9d80b834b88f7ec01f6d0
fatemeh <babaei.f1996@gmail.com>
2024-01-26 06:37:32 +0330

    feat: header

--- my-app/src/components/Partials/Header/Header.jsx
+++ my-app/src/components/Partials/Header/Header.jsx
@@ -1,34 +1,45 @@
 import { BiSearch } from "react-icons/bi";
 import { IoNotificationsOutline } from "react-icons/io5";
 import { MdOutlineDateRange } from "react-icons/md";
+import { CiCirclePlus } from "react-icons/ci";
 
 const Header = ({ isShow, sidebarHandler }) => {
     return (
-        <div className="header">
-            <div>
-                <h2>Welcome back, Vincent 👋</h2>
-                <button
-                    onClick={sidebarHandler}
-                    className={`${
-                        isShow ? "d-none" : "d-block"
-                    } sidebar-btn-handler d-flex`}
-                >
-                    +
-                </button>
-            </div>
-            <div className="header-box">
-                <button className="btn">
-                    <BiSearch />
-                </button>
+        <header>
+            <div className="header">
                 <div>
-                    <MdOutlineDateRange />
-                    <span className="text-gray">19 May 2022</span>
+                    <h2>Welcome back, Vincent 👋</h2>
+                    <button
+                        onClick={sidebarHandler}
+                        className={`${
+                            isShow ? "d-none" : "d-block"
+                        } sidebar-btn-handler d-flex`}
+                    >
+                        +
+                    </button>
                 </div>
-                <button className="btn">
-                    <IoNotificationsOutline />
+                <div className="header-box">
+                    <button className="btn">
+                        <BiSearch />
+                    </button>
+                    <div>
+                        <MdOutlineDateRange />
+                        <span className="text-gray">19 May 2022</span>
+                    </div>
+                    <button className="btn">
+                        <IoNotificationsOutline />
+                    </button>
+                </div>
+            </div>
+            <div className="board-view">
+                <h3>Board view</h3>
+
+                <button className="btn fs-16 fw-600">
+                    <CiCirclePlus className="fs-16 fw-600" />
+                    <span>Add view</span>
                 </button>
             </div>
-        </div>
+        </header>
     );
 };
 
